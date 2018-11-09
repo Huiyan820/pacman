49f5b11fd06c17a6cff3041a62b24498a1b955d5
Huiyan820 <huiyan.chen.2015@sis.smu.edu.sg>
2018-11-09 19:21:38 +0800

    added project

---
+++ MiniProject/MiniProject/Pacman_MiniProject/ghostfeatureExtractors.py
@@ -0,0 +1,112 @@
+# featureExtractors.py
+# --------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"Feature extractors for Ghost Agent game states"
+
+from game import Directions, Actions
+import util
+
+
+class GhostFeatureExtractor:
+    def getFeatures(self, state, action):
+        """
+          Returns a dict from features to counts
+          Usually, the count will just be 1.0 for
+          indicator functions.
+        """
+        util.raiseNotDefined()
+
+
+class GhostIdentityExtractor(GhostFeatureExtractor):
+    def getFeatures(self, state, action):
+        feats = util.Counter()
+        feats[(state, action)] = 1.0
+        return feats
+
+
+def closestFood(pacman_pos, food, walls):
+    fringe = [(pacman_pos[0], pacman_pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        if food[pos_x][pos_y]:
+            return dist
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    return None
+
+
+def closestCapsule(pacman_pos, capsules, walls):
+    fringe = [(pacman_pos[0], pacman_pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        if (pos_x, pos_y) in capsules:
+            return dist
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    return None
+
+
+def stepDistance(from_pos, to_pos, walls):
+    fringe = [(from_pos[0], from_pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        if (pos_x, pos_y) == (int(to_pos[0]), int(to_pos[1])):
+            return dist
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    return None
+
+
+
+
+
+class GhostAdvancedExtractor(GhostFeatureExtractor):
+    
+    
+    def getFeatures(self, state, action):
+
+        x, y = state.getGhostPosition(2)
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        pacmanPosition = state.getPacmanPosition()
+        walls = state.getWalls()
+
+
+        features = util.Counter()
+      
+
+        #Feature 1: distance to pacman
+        #--------------------------------------------------------------------------------------------------
+        distToPacman = stepDistance((next_x, next_y),pacmanPosition, walls)
+        if distToPacman is not None:
+            features["closest-pacman"] = float(distToPacman) / (walls.width * walls.height)
+
+        features.divideAll(10.0)
+        return features
---
+++ MiniProject/MiniProject/Pacman_MiniProject/qlearningGhostAgents.py
@@ -0,0 +1,141 @@
+# qlearningGhostAgents.py
+# ------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+from game import *
+from learningGhostAgents import ReinforcementGhostAgent
+from ghostfeatureExtractors import *
+import sys
+import random,util,math
+import pickle
+class QLearningGhostAgent(ReinforcementGhostAgent):
+    """
+      Q-Learning Ghost Agent
+
+      Functions you should fill in:
+        - computeValueFromQValues
+        - computeActionFromQValues
+        - getQValue
+        - getAction
+        - update
+
+      Instance variables you have access to
+        - self.epsilon (exploration prob)
+        - self.alpha (learning rate)
+        - self.discount (discount rate)
+
+      Functions you should use
+        - self.getLegalActions(state)
+          which returns legal actions for a state
+    """
+    def __init__(self,epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0,agentIndex=1, extractor='GhostIdentityExtractor', **args):
+        "You can initialize Q-values here..."
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['numTraining'] = numTraining
+        args['agentIndex'] = agentIndex
+        self.index = agentIndex 
+        self.q_values = util.Counter()
+        
+        self.featExtractor = util.lookup(extractor, globals())()
+        self.weights = util.Counter()
+        ReinforcementGhostAgent.__init__(self, **args)
+        
+
+    def getQValue(self, state, action):
+        """
+          Returns Q(state,action)
+          Should return 0.0 if we have never seen a state
+          or the Q node value otherwise
+        """
+        qValue= 0.0
+        features = self.featExtractor.getFeatures(state, action)
+        for key in features.keys():
+           	qValue += (self.weights[key] * features[key])
+        
+        return qValue
+
+    def computeValueFromQValues(self, state):
+        possibleActions = self.getLegalActions(state)
+        if possibleActions:
+            maxv = float("-inf")
+            for action in possibleActions:
+                q = self.getQValue(state, action)
+                if q >= maxv:
+                    maxv = q
+            return maxv
+        return 0.0
+        
+    def computeActionFromQValues(self, state):
+        possibleActions = self.getLegalActions(state)
+        if possibleActions:
+            maxv = float("-inf")
+            bestAction = None
+            for action in possibleActions:
+                q = self.getQValue(state, action)
+                if q >= maxv:
+                    maxv = q
+                    bestAction = action
+            return bestAction
+        return None
+        
+    def getWeights(self):
+        return self.weights
+
+
+    def update(self, state, action, nextState, reward):
+        features = self.featExtractor.getFeatures(state, action)          
+        #self.q_values[(state, action)] += self.alpha * (reward + self.discount * self.q_values[(nextState,self.computeActionFromQValues(nextState))] - self.q_values[(state,action)])
+        possibleStateQValues = []
+        for act in self.getLegalActions(state):
+    	    possibleStateQValues.append(self.getQValue(state, act))
+        for key in features.keys():
+            self.weights[key] += self.alpha*(reward+self.discount*((1-self.epsilon)*self.q_values[(nextState,self.computeActionFromQValues(nextState))]+(self.epsilon/len(possibleStateQValues))*(sum(possibleStateQValues)))- self.q_values[(state,action)]) * features[key]
+
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        ReinforcementGhostAgent.final(self, state) 
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            print(self.agentIndex,"  :", self.weights)
+            #sys.exit(1)
+            pass
+
+    def getAction(self, state):
+        #Uncomment the following if you want one of your agent to be a random agent.
+        action = None
+        #if self.agentIndex == 1:
+            #action = random.choice(self.getLegalActions(state))
+        #else: 
+        if random.uniform(0,1) < self.epsilon:
+            action = random.choice(self.getLegalActions(state))
+        else:
+            action = self.computeActionFromQValues(state)
+
+        self.doAction(state, action)
+        return action
+  
+
+    def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+    def getValue(self, state):
+        return self.computeValueFromQValues(state)
+
+
+    
